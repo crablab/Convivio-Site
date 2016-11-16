16a8202d1cddd56593d9b49ee82ccd9810372354
lewisnyman <hello@lewisnyman.co.uk>
2016-11-16 17:21:28 +0000

    Responsive header

--- source/js/index.js
+++ source/js/index.js
@@ -2,6 +2,7 @@
 
 var autosize = require('autosize');
 var inputFocusLabel = require('./input-focus-label');
+var responsiveNav = require('./responsive-nav');
 
 inputFocusLabel(document.querySelectorAll('.text-input'));
 autosize(document.querySelectorAll('textarea'));
---
+++ source/js/responsive-nav.js
@@ -0,0 +1,9 @@
+var priorityNav = require('priority-nav');
+
+var nav = priorityNav.init({
+  navDropdownClassName: 'site-nav__dropdown',
+  navDropdownToggleClassName: 'site-nav__dropdown-toggle',
+  navDropdownLabel: 'More',
+  navDropdownBreakpointLabel: 'Menu',
+  breakPoint: 450
+});
