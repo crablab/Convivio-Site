864beb69378c19054b4e7c3e547904ca2430ae97
lewisnyman <hello@lewisnyman.co.uk>
2016-11-16 18:20:24 +0000

    responsive nav only runs on main menu

--- source/js/responsive-nav.js
+++ source/js/responsive-nav.js
@@ -1,6 +1,7 @@
 var priorityNav = require('priority-nav');
 
 var nav = priorityNav.init({
+  mainNav: 'ul.site-nav',
   navDropdownClassName: 'site-nav__dropdown',
   navDropdownToggleClassName: 'site-nav__dropdown-toggle',
   navDropdownLabel: 'More',
