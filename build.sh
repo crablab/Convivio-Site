d0ffbce6846447d514c19629cf680c801cf425aa
Mike Bell <hello@mikebell.io>
2016-11-24 17:35:15 +0000

    Add load for nvm

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source /var/lib/jenkins/.rvm/scripts/rvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 rvm use 2.2.1
 nvm use
 bundle install
